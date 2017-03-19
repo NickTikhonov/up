e73af963343598640349e88d7dc95712bd8d22d0
Alex Ungurianu <iau2@st-andrews.ac.uk>
2017-03-19 04:48:36 +0000

    Add errors

---
+++ src/errors.js
@@ -0,0 +1,8 @@
+"use strict"
+
+module.exports = {
+    NO_SECRET:"Dropbox API key not found: define the UP_DROPBOX_KEY enviroment variable",
+    FILE_UPLOAD_CONFLICT: "Uploaded file conflicted with an existing file. Please try again",
+    UNKNOWN_ERROR: "Unhandled error: Please contact a developer at https://github.com/NickTikhonov/up",
+    PROVIDER_NOT_FOUND: "Could not find specified provider"
+}
