799d039281b753af87e0d32dc05f1f750d308f29
Alex Ungurianu <iau2@st-andrews.ac.uk>
2017-03-19 06:06:43 +0000

    Implement gist upload

---
+++ src/providers/gist.js
@@ -0,0 +1,41 @@
+"use strict"
+
+const path = require("path")
+
+const request = require("request")
+
+function genRequestJson(filePath) {
+  var fileContent = fs.readFileSync(filePath, 'utf-8')
+  var reqJson = {
+    description: "Uploaded using up (https://github.com/NickTikhonov/up)",
+    public:false,
+    files: {}
+  }
+
+  reqJson.files[path.basename(filePath)] = {content: fileContent}
+
+  return reqJson
+}
+
+function upload(path) {
+  return new Promise(function(resolve, reject) {
+
+    request.post({
+        url:"https://api.github.com/gists",
+        headers: {
+          "User-Agent": "up v0.1"
+        },
+        json: genRequestJson(path)
+      },
+      function (err, response, body) {
+        if(err) {
+          reject(errors.UNKNOWN_ERROR)
+        }
+        else {
+          resolve(body.url)
+        }
+      })
+  })
+}
+
+module.exports = upload
