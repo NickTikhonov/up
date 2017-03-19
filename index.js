bce5093baf587b4c32e13690d02c3fdf12ea224d
Nick Tikhonov <nt34@st-andrews.ac.uk>
2017-03-19 02:04:26 +0000

    Add pipe handling

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 #!/usr/bin/env node
+"use strict"
+
+var fs = require("fs")
 var program = require("commander")
+var tmp = require("tmp")
 
 program
-  .arguments('<file..>')
+  .arguments("<file..>")
   .parse(process.argv)
 
 if (program.args.length === 0) {
-  console.error("please provide at least one <file> argument.")
+  tmp.file(function(err, path, fd, cleanup) {
+    if (err) {
+      console.log(err)
+    }
+
+    writeInputToFile(path)
+      .then(function() {
+        //TODO: upload here
+      })
+      .catch(function(err) {
+        //TODO: die here
+        console.log(err)
+      })
+  })
+} else {
+  program.args.forEach(function(path) {
+    if (!validPath(path)) {
+      //TODO: die here
+    }
+
+    //TODO: upload here
+  })
+}
+
+function validPath(path) {
+  if (fs.existsSync(path)) {
+    var stats = fs.lstatSync(path)
+    if (stats.isFile()) {
+      return true
+    }
+  }
+
+  return false
+}
+
+function writeInputToFile(filePath) {
+  return new Promise(function(resolve, reject) {
+    var streamData = ""
+
+    process.stdin.resume();
+    process.stdin.on('data', function(buf) {
+      streamData += buf.toString();
+    });
+
+    process.stdin.on('end', function() {
+      fs.writeFile(filePath, streamData, function(err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      })
+    });
+  })
 }
