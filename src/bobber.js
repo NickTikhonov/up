e1b1e6a63b7f8294f37b85d20be7eb8440da6ec7
Alex Ungurianu <iau2@st-andrews.ac.uk>
2017-03-20 20:23:26 +0000

    Fix bobber breaking when piping the output

--- src/bobber.js
+++ src/bobber.js
@@ -55,23 +55,23 @@ class Bobber {
   }
 
   bob() {
-    process.stdout.cursorTo(0)
-    process.stdout.write(this.ANIMATION[this.frameIdx])
+    process.stderr.cursorTo(0)
+    process.stderr.write(this.ANIMATION[this.frameIdx])
     this.frameIdx = (this.frameIdx + 1) % this.ANIMATION.length
   }
 
   start() {
-    if (process.stdout) {
-      process.stdout.write("\x1b[34m")
+    if (process.stderr && process.stderr.isTTY) {
+      process.stderr.write("\x1b[34m")
       this.interval = setInterval(() => this.bob(), 50)
     }
   }
 
   stop() {
-    if (process.stdout) {
-      process.stdout.clearLine()
-      process.stdout.cursorTo(0)
-      process.stdout.write("\x1b[0m")
+    if (process.stderr && process.stderr.isTTY) {
+      process.stderr.clearLine()
+      process.stderr.cursorTo(0)
+      process.stderr.write("\x1b[0m")
       clearInterval(this.interval)
     }
   }
